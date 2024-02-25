038a1f362f5d4d0e651da4e00352f7416226d498
Jonathan Gates <jonathangates33@gmail.com>
2024-02-25 16:36:25 -0700

    Basic menuing

---
+++ GameStateEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace CS5410
+{
+    public enum GameStateEnum
+    {
+        MainMenu,
+        GamePlay,
+        HighScores,
+        Controls,
+        Credits,
+        Exit
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new LunarLander.Lander();
+game.Run();
